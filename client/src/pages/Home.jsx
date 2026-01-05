e935b0a560f43df9e4b6fe2c1e9f38b31ec23a71
Nick <njaginicholas@gmail.com>
2026-01-05 17:00:02 +0300

    Adjust margin and padding in Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
     const { currentUser,} = useContext(UserContext)
 
   return (
-    <main className="px-4 my-12">
+    <main className="px-4 my-10">
       <div className="relative flex mx-auto justify-center items-center h-[330px] md:h-[400px] w-full max-w-[1254px] rounded-2xl bg-[url('./assets/images/banner-bg.jpg')] bg-[linear-gradient(to bottom,rgba(0,0,0,0,0.5))] bg-blend-overlay bg-no-repeat bg-cover bg-center after:absolute after:h-full after:w-full after:top-0 after:left-0 after:bg-overlayDark after:rounded-2xl after:z-10 ">
         <h1 className="relative md:px-10 text-center text-3xl md:text-5xl font-semibold text-white z-30">
           Snag Your Spot, Stress-Free – Reservations, Ready, Set, Go!
         </h1>
       </div>
-      <div className="mx-auto my-12 max-w-[1254px]">
-        <p className="w-full my-10 text-2xl font-semibold max-w-2xl">
+      <div className="mx-auto my-10 max-w-[1254px]">
+        <p className="w-full mb-10 text-2xl font-semibold max-w-2xl">
           Got a culinary masterpiece to share? Perhaps you crave diversity in
           your daily servings? Our community is eagerly awaiting - jump in!
         </p>
